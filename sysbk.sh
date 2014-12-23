d34deb95eff14f1f68446a00344e0740486b4d90
fenggee007 <fenggee007@163.com>
2014-12-23 23:05:41 +0800

    second commit

--- sysbk.sh
+++ sysbk.sh
@@ -3,7 +3,7 @@
 sp="/mnt/data"
 #echo "$sp"
 #mount dev to save backup files
-mount /dev/sda6 $sp >/dev/null 2>&1
+mount /dev/example $sp >/dev/null 2>&1
 
 #change directory of dev
 topath="$sp/Backup/fedora/sysbk"
@@ -17,7 +17,7 @@ then
     #echo $na
     log="$ti""f21sysbk.log"
     #backup operation
-    homewk="/home/clf"
+    homewk="/home/example"
     tar cvPzf $na / --exclude=/dev --exclude=/lost+found --exclude=/media --exclude=/mnt --exclude=/proc --exclude=/sbin --exclude=/sys --exclude=/tmp --exclude=/var --exclude=$homewk/Documents --exclude=$homewk/Downloads --exclude=$homewk/Music --exclude=$homewk/Pictures --exclude=$homewk/Videos --exclude=$homewk/Work --exclude=/home/lost+found --exclude=/run > $log
 
     endtime=`date "+%Y%m%d%H%M"`
