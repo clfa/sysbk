58709d9746ed8a739604cf023eb4ad3177c4102d
fenggee007 <fenggee007@163.com>
2014-12-23 23:12:39 +0800

    third commit

--- sysbk.sh
+++ sysbk.sh
@@ -1,21 +1,21 @@
 ﻿#!/bin/bash
 
-sp="/mnt/data"
-#echo "$sp"
+sp="/mnt/example"
+
 #mount dev to save backup files
 mount /dev/example $sp >/dev/null 2>&1
 
 #change directory of dev
-topath="$sp/Backup/fedora/sysbk"
+topath="$sp/example"
 cd $topath
 path=`pwd`
 if [[ $path = $topath ]]
 then
     ti=`date "+%Y%m%d%H%M"`
     echo "Start time: $ti"
-    na="$ti""f21sysbk.tgz"
+    na="$ti""sysbk.tgz"
     #echo $na
-    log="$ti""f21sysbk.log"
+    log="$ti""sysbk.log"
     #backup operation
     homewk="/home/example"
     tar cvPzf $na / --exclude=/dev --exclude=/lost+found --exclude=/media --exclude=/mnt --exclude=/proc --exclude=/sbin --exclude=/sys --exclude=/tmp --exclude=/var --exclude=$homewk/Documents --exclude=$homewk/Downloads --exclude=$homewk/Music --exclude=$homewk/Pictures --exclude=$homewk/Videos --exclude=$homewk/Work --exclude=/home/lost+found --exclude=/run > $log
